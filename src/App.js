7adefae3f42a715860866a2a5002a295bc84bb5e
akxaalex16 <akxaalex16@gmail.com>
2021-06-11 01:34:43 -0700

    Installed Bootstrap in terminal, added h1 to App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1>Dictionary</h1>
+    </div>
+  );
 }
 
 export default App;
