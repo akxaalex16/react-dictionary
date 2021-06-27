9b8f8dab3bac4a98c45e9d6dbad89999d6e19fb6
akxaalex16 <akxaalex16@gmail.com>
2021-06-27 03:02:52 -0700

    Made API call for the photos and used new state in Dictionary.js, added new component Photos.js to display the mapped/looped images, and styled the images in Photos.css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -2,20 +2,33 @@ import React, { useState } from "react";
 import "./Dictionary.css";
 import axios from "axios";
 import Results from "./Results";
+import Photos from "./Photos";
 
 export default function Dictionary(props) {
   const [keyword, setKeyword] = useState(props.defaultWord);
   const [searchedWord, setSearchedWord] = useState(null);
   const [loaded, setLoaded] = useState(false);
+  const [photo, setPhoto] = useState(null);
 
-  function handleResponse(response) {
+  function handleDictionaryResponse(response) {
     setSearchedWord(response.data[0]);
   }
 
+  function handlePexelResponse(response) {
+    console.log(response.data.photos);
+    setPhoto(response.data.photos);
+  }
   function search() {
     //documentation: https://dictionaryapi.dev/
     let apiUrl = `https://api.dictionaryapi.dev/api/v2/entries/en_US/${keyword}`;
-    axios.get(apiUrl).then(handleResponse);
+    axios.get(apiUrl).then(handleDictionaryResponse);
+
+    //documentation: https://www.pexels.com/api/documentation/?
+    let pexelsApiKey =
+      "563492ad6f91700001000001b64d435d9b3942c287aac9982bab6738";
+    let pexelsUrl = `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
+    let headers = { Authorization: `Bearer ${pexelsApiKey}` };
+    axios.get(pexelsUrl, { headers: headers }).then(handlePexelResponse);
   }
 
   function handleSubmit(event) {
@@ -50,6 +63,7 @@ export default function Dictionary(props) {
           </div>
         </section>
         <Results meanings={searchedWord} />
+        <Photos photos={photo} />
       </div>
     );
   } else {
